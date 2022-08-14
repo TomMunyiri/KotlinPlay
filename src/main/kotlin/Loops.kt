1e2a373518e72d6a66882522f205d881fc9e96a7
tom_munyiri <munyiri.thomas@eclectics.io>
2022-08-14 16:49:53 +0300

    loop constructs

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -1,7 +1,23 @@
-fun main(){
+import java.util.TreeMap
+
+fun main() {
     //for loop with range. this will include even 10 in the loop
-    for(x in 1..10) println(x)
+    for (x in 1..10) println(x)
 
     //this will exclude 10
-    for(x in 1 until 10) println(x)
+    for (x in 1 until 10) println(x)
+
+    for (x in 1..10 step 2) println(x)
+
+    for (x in 10 downTo 1) println(x)
+
+    val ages = TreeMap<String, Int>()
+    ages["Tom"] = 25
+    ages["Liz"] = 24
+    ages["Eunice"] = 55
+    ages["Edwin"] = 30
+    ages["Rocky"] = 89
+    for ((name, age) in ages) {
+        println("Name: $name. Age: $age")
+    }
 }
