571174e5049f01db9fbeeb0be03397aef95db854
tom_munyiri <munyiri.thomas@eclectics.io>
2022-10-29 19:46:52 +0300

    kotlin scope functions

--- src/main/kotlin/ScopeFunctions.kt
+++ src/main/kotlin/ScopeFunctions.kt
@@ -1,15 +1,37 @@
+/**
+ * Referenced this article by Banda https://effbada.hashnode.dev/understanding-kotlin-scope-functions-ckcfxb0c000093cs10gsdcvhe
+ */
 data class Animal(
-    val name: String = "Panda",
-    val colour: String = "Black",
-    val origin: String = "Greenland",
+    var name: String = "Panda",
+    var colour: String = "Black",
+    var origin: String? = "Greenland",
 )
 
 fun main() {
-
+    letExample()
 }
 
 fun letExample() {
+    /**
+     * By default, the last statement in a let block is a return statement if it is not an assignment
+     */
+    val animal = Animal().let {
+        //this is a string so it will return a string
+        "This animal is from ${it.origin}"
+    }
+    println(animal)
+    val animal1 = Animal().let {
+        //this is an assignment so it will return kotlin.Unit
+        //we are assigning the origin to a new value and are not returning anything
+        it.origin = "Kenya"
+        //if we did this, a string will be returned with the origin having been updated to the new value
+        //"This animal is from ${it.origin}"
+    }
+    //let can help us ensure null safety thus saving us from the lengthy null checks
+    val animal2 = Animal().origin?.let {
 
+    }
+    println(animal1)
 }
 
 fun applyExample() {
