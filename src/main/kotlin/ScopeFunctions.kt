9ceb3eeb925d5d1907d8959a5f2b54bafe871695
tom_munyiri <munyiri.thomas@eclectics.io>
2022-10-29 17:40:16 +0300

    kotlin scope functions

---
+++ src/main/kotlin/ScopeFunctions.kt
@@ -0,0 +1,29 @@
+data class Animal(
+    val name: String = "Panda",
+    val colour: String = "Black",
+    val origin: String = "Greenland",
+)
+
+fun main() {
+
+}
+
+fun letExample() {
+
+}
+
+fun applyExample() {
+
+}
+
+fun withExample() {
+
+}
+
+fun alsoExample() {
+
+}
+
+fun runExample() {
+
+}
