2ea23732e415a92e3f6066796ce3d8aa84216034
Tom Munyiri <tmmunyiri@gmail.com>
2023-05-01 01:51:24 +1100

    remove suffix combined woth map, remove surrounding

--- src/main/kotlin/Lists.kt
+++ src/main/kotlin/Lists.kt
@@ -1,5 +1,6 @@
 fun main() {
     square()
+    removeSuffix()
 }
 
 private fun square() {
@@ -13,6 +14,23 @@ private fun square() {
     //the simplified way with map
     val squareMap = (1..10).map { it * it }
     println(squareMap)
+}
+
+private fun removeSuffix() {
+    data class Fruit(val name: String, val sugar: Int, val photo: String)
+
+    val fruits = listOf(
+        Fruit("mango", 12, "mango.jpg"),
+        Fruit("orange", 34, "orange.jpg"),
+        Fruit("passion", 45, "passion.jpg"),
+        Fruit("lemon", 78, "lemon.jpg"),
+        Fruit("apple", 2, "apple.jpg"),
+        Fruit("peach", 16, "peach.jpg"),
+        Fruit("water melon", 17, "water melon.jpg"),
+    ).sortedBy(Fruit::sugar)
+
+    println(fruits.map { it.photo.removeSuffix(".jpg") })
+    println("__MORE__".removeSurrounding("__"))
 }
 
 
