06cb4df44a44a7fd801b1b9f7034ae67b4d7d937
Thomas Munyiri <tmmunyiri@gmail.com>
2025-02-18 13:23:39 +0300

    grooking algorithms

---
+++ src/main/kotlin/dsa/grookingAlgorithms/BinarySearch.kt
@@ -0,0 +1,24 @@
+package dsa.grookingAlgorithms
+
+fun List<Int>.binarySearch(item: Int) : Int? {
+    var min = 0
+    var max = lastIndex
+    while (min <= max) {
+        val mid = (min + max)/2
+        val guess = this[mid]
+        if (guess == item) return mid
+        if (guess > item) max = mid -1
+        else min = mid + 1
+    }
+    return null
+}
+
+
+val sortedArray = (0..100 step 3).toList()
+
+
+fun main(args: Array<String>) {
+    println(sortedArray)
+    print(sortedArray.binarySearch(6))
+}
+
